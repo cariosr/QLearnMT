f64b59196b8627344963d8733459b4cf6c07dda8
rbucchia <mb226@sogo.uni-heidelberg.de>
2020-03-10 15:18:27 +0100

    Fixed preprocessing error handling

--- Preprocessing/preprocess_IWSLT15.sh
+++ Preprocessing/preprocess_IWSLT15.sh
@@ -16,12 +16,18 @@ merge_ops=32000
 src=$1
 tgt=$2
 lang=$3
+
 if [ -z "$1" ]; then
     echo "Missing source language argument. Script has three arguments: src tgt src-tgt."
+fi
+
 if [ -z "$2" ]; then
     echo "Missing target language argument. Script has three arguments: src tgt src-tgt."
+fi
+
 if [ -z "$3" ]; then
     echo "Missing language pair argument. Script has three arguments: src tgt src-tgt."
+fi
 
 URL="https://wit3.fbk.eu/archive/2015-01/texts/${src}/${tgt}/${lang}.tgz"
 prep="../test/data/iwslt15/${lang}"
